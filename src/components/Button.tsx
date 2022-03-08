e468a9f6685dd5f4d65eada3d56459473dc9ed8b
Rafael Lopes <rafael.miranda.lopes@usp.br>
2022-03-08 03:06:37 -0300

    added firebase and .env

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { Children } from "react";
+import { Children, useState } from "react";
 
 type ButtonProps = {
   text?: string;
@@ -6,7 +6,18 @@ type ButtonProps = {
 }
 
 export function Button(props: ButtonProps) {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <button>{ (props.text || props.children?.at(1) || 'default')}</button>
+    <button
+      onClick={increment}
+    >{ 
+      (counter || props.text || props.children?.at(1) || 'default')
+      }
+    </button>
   )
 }
